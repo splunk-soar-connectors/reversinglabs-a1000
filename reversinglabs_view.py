0dcc68e3ddefc8652fea44979ea613598548a6b6
erni <erni.fekete@reversinglabs.com>
2018-03-21 11:37:43 +0100

    fixed visualization mapping issue

--- reversinglabs_view.py
+++ reversinglabs_view.py
@@ -17,43 +17,46 @@ def file_reputation(provides, all_results, context):
     print all_results
     results = []
     parameters = {}
-    str_class = ['Unknown', 'Known', 'Suspicious', 'Malicious']
-    # report[0]['data'][0]['task_info']['report'][0]['classification']
+
     for summary, action_results in all_results:
-        print "summary " + str(summary) + " action_results " + str(action_results)
         if not summary or not action_results:
             continue
-        for result in action_results:
-            parameter = result.get_param()
-            result_summary = result.get_summary()
-            print "result " + str(result_summary) + " parameter " + str(parameter)
-            for dataelem in result.get_data():
-                # print " ***************************** data element  : " + str(dataelem) + "****************************"
-                if 'a1000_link' in dataelem:
-                    print "++++++++++++++++++++ a1000_link +++++++++++++++ :" + dataelem['a1000_link']
-                    parameters['a1000_link'] = dataelem['a1000_link']
-                if 'task_info' in dataelem:
-                    print " ***************************** task_info  ***************************"
-                    for report in dataelem['task_info']['report']:
-                        if 'classification' in report:
-                            print "++++++++++++++++++++ classification +++++++++++++++ :" + str(report['classification'])
-                            if 'threat_factor' in report['classification']:
-                                parameters['threat_factor'] = report['classification']['threat_factor']
-                            else:
-                                parameters['threat_factor'] = " Check A1000 report "
-                            if 'status' in report['classification']:
-                                parameters['status'] = str_class[report['classification']['classification']]
-                            else:
-                                parameters['status'] = " Analyzed "
-                            # put together the scan results
-                            scan_results = " Please click A1000 link to view the report "
-                            if 'scan_results' in report['classification']:
-                                for scan in report['classification']['scan_results']:
-                                    scan_results = scan_results + "Classifier " + scan['name'] + " found threat named "
-                                    scan_results = scan_results + scan['result'] + " with threat factor " + str(scan['threat_factor'])
-                                    scan_results = scan_results + " and marked file as " + str_class[scan['classification']] + ". \n"
-                            parameters['scanners'] = scan_results
-                            print str(parameters)
+
+        try:
+            result = action_results[0].get_data()[0]
+        except:
+            result = {}
+        try:
+            result_summary = action_results[0].get_summary()
+        except:
+            result_summary = {}
+
+        if 'a1000_report_url' in result_summary:
+            parameters['a1000_link'] = result_summary["a1000_report_url"]
+        else:
+            parameters['a1000_link'] = 'http://www.reversinglabs.com'
+
+        if 'threat_level' in result:
+            parameters['threat_level'] = result["threat_level"]
+        else:
+            parameters['threat_level'] = "Check A1000 report"
+
+        if 'trust_factor' in result:
+            parameters['trust_factor'] = result["trust_factor"]
+        else:
+            parameters['trust_factor'] = "Check A1000 report"
+
+        if 'threat_status' in result:
+            parameters['status'] = result["threat_status"] #str_class[report['classification']['classification']]
+        else:
+            parameters['status'] = "Analyzed"
+
+        parameters['scanners'] = "Please click A1000 link to view the report"
+
+
+
+
+
     context['parameters'] = parameters
     context['results'] = results
     context['title_text_color'] = 'white'
